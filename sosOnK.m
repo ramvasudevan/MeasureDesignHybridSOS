d83db61f40d1a9341462bac1df6714c9b180ddc3
snmohan <elemsn@umich.edu>
2015-03-05 15:31:31 -0500

    Enforcing max degree of multiplying polynomials in Psatz

--- sosOnK.m
+++ sosOnK.m
@@ -14,10 +14,24 @@
 function [ prg, tk ] = sosOnK( prg, p, x, h, d )
     m = size( h, 1 ); % total number of h's
     S = msspoly( zeros( m, 1 ) ); % create this many multiplier variables
-    bases = monomials( x, 0:d );
     for i = 1:m
+        
+        % choosing the multipler polynomials to be the right degree
+        [Vexp,Pexp,~]=decomp(h(i));
+        tmp1=match(Vexp,x);
+        f=find(tmp1~=0);
+        Ptmp=Pexp(:,tmp1(f));
+        d_tmp=floor((d-full(max(sum(Ptmp,2))))/2)*2;
+        
+        % uncomment if you want to use the provided degree
+%         d_tmp=d;
+        
+        bases = monomials( x, 0:d_tmp);
         [ prg, S( i ) ] = prg.newFreePoly( bases ); % make a polynomial whose coefficients are free variables
         prg = prg.withSOS( S( i ) ); % ensure that the polynomial is SOS
     end
-    [ prg, tk ] = prg.withSOS( p - S'*h ); % ensure that p is SOS on K 
+%     keyboard;
+    [ prg, tk ] = prg.withSOS( p - S'*h ); % ensure that p is SOS on K  % Punitar's Positivstellansatz
+%     prg.sosVariable=length(prg.)
+    prg.sosTokens=[prg.sosTokens;tk]; % storing the tokens corresponding to the desires SOS constraint
 end
